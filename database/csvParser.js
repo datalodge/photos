535df34f3a8d4e83ec5c4bfb6a7b75254d151fe3
paul-do <pauldo247@gmail.com>
2019-03-04 12:06:53 -0800

    Update git ignore file

--- database/csvParser.js
+++ database/csvParser.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const csv = require('fast-csv');
 
-var stream = fs.createReadStream("data.csv");
+var stream = fs.createReadStream("dataUUID.csv");
  
 var csvStream = csv()
     .on("data", function(data){
-         console.log(data);
+         console.log(data)
     })
     .on("end", function(){
          console.log("done");
     });
  
 stream.pipe(csvStream);
+
