9277e9fdafad4e995cd3b1af4a00dbe6230cf84f
paul-do <pauldo247@gmail.com>
2019-03-04 15:27:47 -0800

    Delete old prior db files / rename Cassandra variables

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const parse = require('body-parser');
 const expressStaticGzip = require('express-static-gzip');
 const db = require('../database/index.js');
-const dbCass = require('../database/indexCassandra.js');
+
 
 
 const app = express();
@@ -22,7 +22,7 @@ app.use('/', expressStaticGzip(path.join(__dirname, '/../client/dist'), {
 }));
 
 app.get('/pictures/:homeId', (req, res) => {
-  dbCass.get(req.params.homeId)
+  db.get(req.params.homeId)
     .then((photos) => {
       photos.forEach((photo) => {
         let s3Path = 'https://s3.us-east-2.amazonaws.com/data-house-photos/';
@@ -37,26 +37,5 @@ app.get('/pictures/:homeId', (req, res) => {
     })
 });
 
-// app.post('/pictures/:homeId', (req, res) => {
-//   // TO-DO POST
-//   db.postPhoto(req.params.homeId, (err, res) => {
-//     if(err){
-//       console.log(err)
-//     } else {
-//       res.send('posted photo')
-//     }
-//   })
-// });
-
-// app.delete('/pictures/:homeId', (req, res) => {
-//   // TO-DO DELETE
-//   db.deletePhoto(req.params.homeId, (err, data) => {
-//     if(err){
-//       console.log(err)
-//     } else {
-//       res.send('successfully deleted a picture')
-//     }
-//   });
-// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
