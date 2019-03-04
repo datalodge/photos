a42ea6dd26849f234855b10582c7d57b6ec4e227
paul-do <pauldo247@gmail.com>
2019-03-04 15:22:47 -0800

    Hook up server with local Cassandra DB

--- database/index.js
+++ database/index.js
@@ -55,3 +55,4 @@ module.exports = {
   deletePhoto,
   postPhoto
 } 
+
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const parse = require('body-parser');
 const expressStaticGzip = require('express-static-gzip');
 const db = require('../database/index.js');
+const dbCass = require('../database/indexCassandra.js');
 
 
 const app = express();
@@ -21,35 +22,41 @@ app.use('/', expressStaticGzip(path.join(__dirname, '/../client/dist'), {
 }));
 
 app.get('/pictures/:homeId', (req, res) => {
-  db.getAll(req.params.homeId, (err, data) => {
-    if (err) {
+  dbCass.get(req.params.homeId)
+    .then((photos) => {
+      photos.forEach((photo) => {
+        let s3Path = 'https://s3.us-east-2.amazonaws.com/data-house-photos/';
+        photo.url = s3Path + 'photos' + photo.url + '.jpg';
+        photo.thumb_url = s3Path + 'thumb_photos' + photo.thumb_url + '.jpg'
+      })
+      console.log(photos)
+      res.send(photos)
+    })
+    .catch((err) => {
       console.log(err);
-    } else {
-      res.json(data);
-    }
-  });
+    })
 });
 
-app.post('/pictures/:homeId', (req, res) => {
-  // TO-DO POST
-  db.postPhoto(req.params.homeId, (err, res) => {
-    if(err){
-      console.log(err)
-    } else {
-      res.send('posted photo')
-    }
-  })
-});
+// app.post('/pictures/:homeId', (req, res) => {
+//   // TO-DO POST
+//   db.postPhoto(req.params.homeId, (err, res) => {
+//     if(err){
+//       console.log(err)
+//     } else {
+//       res.send('posted photo')
+//     }
+//   })
+// });
 
-app.delete('/pictures/:homeId', (req, res) => {
-  // TO-DO DELETE
-  db.deletePhoto(req.params.homeId, (err, data) => {
-    if(err){
-      console.log(err)
-    } else {
-      res.send('successfully deleted a picture')
-    }
-  });
-});
+// app.delete('/pictures/:homeId', (req, res) => {
+//   // TO-DO DELETE
+//   db.deletePhoto(req.params.homeId, (err, data) => {
+//     if(err){
+//       console.log(err)
+//     } else {
+//       res.send('successfully deleted a picture')
+//     }
+//   });
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
